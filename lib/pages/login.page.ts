c70cfb6368855a073f50a8d68c83a6629fb27516
Jeremy Polley <jeremypolley@gmail.com>
2024-07-10 10:46:09 -0400

    Dismiss cookies if visible.

--- lib/pages/login.page.ts
+++ lib/pages/login.page.ts
@@ -1,10 +1,11 @@
 import { Page } from "@playwright/test";
 
 export class LoginPage {
-  readonly email = this.page.getByLabel('Email');
+  readonly email = this.page.getByLabel("Email");
   readonly password = this.page.getByLabel("Password");
   readonly loginButton = this.page.getByRole("button", { name: "Log in" });
   readonly invalidCredentialsError = this.page.getByText("Email / username or password is incorrect.");
+  readonly dismissCookiesElement = this.page.getByRole("button", { name: "Close" });
 
   async goto() {
     await this.page.goto("https://bitly.com/a/sign_in");
@@ -13,11 +14,18 @@ export class LoginPage {
   async login(email: string, password: string) {
     await this.email.fill(email);
     await this.password.fill(password);
+    await this.dismissCookies();
     await this.loginButton.click();
   }
 
   async loginWithValidCredentials() {
     await this.login(process.env.USER_EMAIL ?? "", process.env.USER_PASSWORD ?? "");
+  }
+
+  async dismissCookies() {
+    if (await this.dismissCookiesElement.isVisible()) {
+      await this.dismissCookiesElement.click();
+    }
   }
 
   constructor(private readonly page: Page) {}
